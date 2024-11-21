2b7fdfd63a362d4202108c528029216a75e08ca6
Justin Schultz <SchultzJustin@ymail.com>
2024-11-21 01:39:18 +0000

    Added some basic functionality to TTT game

--- projects/tic_tac_toe.py
+++ projects/tic_tac_toe.py
@@ -1,31 +1,32 @@
+import random
+
 class Board:
     def __init__(self):
-        self.squares = {
-            1: "X",
-            2: " ",
-            3: " ",
-            4: " ",
-            5: "O",
-            6: " ",
-            7: " ",
-            8: "O",
-            9: "X",
-        }
+        self.squares = {key: Square() for key in range(1, 10)}
 
     def display(self):
         print()
         print("     |     |")
-        print(f"  {self.squares[1]}  |  {self.squares[2]}  |  {self.squares[3]}")
+        print(f"  {self.squares[1]}  |"
+              f"  {self.squares[2]}  |"
+              f"  {self.squares[3]}")
         print("     |     |")
         print("-----+-----+-----")
         print("     |     |")
-        print(f"  {self.squares[4]}  |  {self.squares[5]}  |  {self.squares[6]}")
+        print(f"  {self.squares[4]}  |"
+              f"  {self.squares[5]}  |"
+              f"  {self.squares[6]}")
         print("     |     |")
         print("-----+-----+-----")
         print("     |     |")
-        print(f"  {self.squares[7]}  |  {self.squares[8]}  |  {self.squares[9]}")
+        print(f"  {self.squares[7]}  |"
+              f"  {self.squares[8]}  |"
+              f"  {self.squares[9]}")
         print("     |     |")
         print()
+
+    def mark_square(self, key, marker):
+        self.squares[key] = marker
 
 
 class Row:
@@ -34,8 +35,15 @@ class Row:
 
 
 class Square:
-    def __init__(self):
-        pass
+    INITIAL_MARKER = " "
+    HUMAN_MARKER = "X"
+    COMPUTER_MARKER = "O"
+
+    def __init__(self, marker=INITIAL_MARKER):
+        self._marker = marker
+
+    def __str__(self):
+        return self._marker
 
 
 class Marker:
@@ -44,29 +52,33 @@ class Marker:
 
 
 class Player:
+    def __init__(self, marker):
+        self.marker = marker
+
+    @property
+    def marker(self):
+        return self._marker
+
+    @marker.setter
+    def marker(self, marker):
+        self._marker = marker
+
+
+class Human(Player):
     def __init__(self):
-        pass
-
-    def mark(self):
-        pass
-
-    def play(self):
-        pass
+        super().__init__(Square.HUMAN_MARKER)
 
 
-class Human:
+class Computer(Player):
     def __init__(self):
-        pass
-
-
-class Computer:
-    def __init__(self):
-        pass
+        super().__init__(Square.COMPUTER_MARKER)
 
 
 class TTTGame:
     def __init__(self):
         self.board = Board()
+        self.human = Human()
+        self.computer = Computer()
 
 
     def play(self):
@@ -74,12 +86,12 @@ class TTTGame:
 
         while True:
             self.board.display()
-            self.player_one_turn()
+            self.human_turn()
 
             if self.is_game_over():
                 break
 
-            self.player_two_turn()
+            self.computer_turn()
 
             if self.is_game_over():
                 break
@@ -96,11 +108,25 @@ class TTTGame:
     def display_goodbye_message(self):
         print("Thanks for playing!")
 
-    def player_one_turn(self):
-        pass
+    def human_turn(self):
+        choice = None
 
-    def player_two_turn(self):
-        pass
+        while True:
+            try:
+                choice = int(input("Please, choose a square (1-9): "))
+                if 1 <= choice <= 9:
+                    break
+            except ValueError:
+                pass
+
+            print("That\'s not a valid square.")
+
+        self.board.mark_square(choice, self.human.marker)
+
+    def computer_turn(self):
+        choice = random.randint(1, 9)
+
+        self.board.mark_square(choice, self.computer.marker)
 
     def is_game_over(self):
         pass
