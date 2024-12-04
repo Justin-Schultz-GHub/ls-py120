ca9481369e339117a36dd9b82ad5a6a537503600
Justin Schultz <SchultzJustin@ymail.com>
2024-12-04 04:14:01 +0000

    refactored and extracted find_vulnerable_square logic to helper methods

--- projects/tic_tac_toe.py
+++ projects/tic_tac_toe.py
@@ -98,6 +98,16 @@ class Computer(Player):
 
 
 class TTTGame:
+    INTERCARDINALS = [1, 3, 7, 9]
+    CARDINALS = [2, 4, 6, 8]
+    MIDDLE_SQUARE = 5
+    SUM_OF_OPPOSITE_CORNERS = 10
+    L_SQUARE_DICT = {
+            1: [(6, 3), (8, 7)],
+            3: [(4, 1), (8, 9)],
+            7: [(2, 1), (6, 9)],
+            9: [(4, 7), (2, 3)],
+        }
     POSSIBLE_WINNING_ROWS = (
         (1, 2, 3),
         (4, 5, 6),
@@ -203,21 +213,39 @@ class TTTGame:
         self.board.mark_square_at(choice, self.computer.marker)
 
     def find_vulnerable_square(self):
-        INTERCARDINALS = [1, 3, 7, 9]
-        CARDINALS = [2, 4, 6, 8]
-        MIDDLE_SQUARE = 5
-        SUM_OF_OPPOSITE_CORNERS = 10
-        L_SQUARE_DICT = {
-            1: [(6, 3), (8, 7)],
-            3: [(4, 1), (8, 9)],
-            7: [(2, 1), (6, 9)],
-            9: [(4, 7), (2, 3)],
-        }
-
         # Priority 1: Check for empty middle square
-        if self.board.squares[MIDDLE_SQUARE].is_unused():
-            return MIDDLE_SQUARE
+        middle_unclaimed = self.is_middle_unclaimed()
+        if middle_unclaimed:
+            return self.MIDDLE_SQUARE
 
+        # Priority 2: Check for computer win or block player
+        win_or_block = self.is_win_or_block()
+        if win_or_block:
+            return win_or_block
+
+        #Priority 3: Check double intercardinals, and block cardinal
+        block_cardinal = self.is_double_intercardinals()
+        if block_cardinal:
+            return block_cardinal
+
+        #Priority 4: Check intercardinal and opposing cardinals, block shared
+        # corner
+        block_corner = self.is_l_square_danger()
+        if block_corner:
+            return block_corner
+
+        # Priority 5: Take corner
+        open_corner = self.open_corner()
+        if open_corner:
+            return open_corner
+
+        # Return None and choose random square (fallback)
+        return None
+
+    def is_middle_unclaimed(self):
+        return self.board.squares[self.MIDDLE_SQUARE].is_unused()
+
+    def is_win_or_block(self):
         for line in self.POSSIBLE_WINNING_ROWS:
             sq1, sq2, sq3 = line
 
@@ -226,7 +254,6 @@ class TTTGame:
                        self.board.squares[sq3].marker
                        ]
 
-            # Priority 2: Check for computer win or block player
             if (
                 (markers.count(Square.COMPUTER_MARKER) == 2
                 or markers.count(Square.HUMAN_MARKER) == 2)
@@ -234,22 +261,32 @@ class TTTGame:
             ):
                 return line[markers.index(Square.INITIAL_MARKER)]
 
-            #Priority 3: Check double intercardinals, and block cardinal
+        return None
+
+    def is_double_intercardinals(self):
+        for line in self.POSSIBLE_WINNING_ROWS:
+            sq1, _, sq3 = line
+
             if (
                 self.board.squares[sq1].marker == Square.HUMAN_MARKER
                 and self.board.squares[sq3].marker == Square.HUMAN_MARKER
-                and sq1 + sq3 == SUM_OF_OPPOSITE_CORNERS
+                and sq1 + sq3 == self.SUM_OF_OPPOSITE_CORNERS
                 and sq1 % 2 == 1
             ):
-                return random.choice(CARDINALS)
+                random_cardinal = self.get_random_cardinal()
+                return random_cardinal
 
-        #Priority 4: Check intercardinal and opposing cardinals, block shared
-        # corner
-        for key, value in L_SQUARE_DICT.items():
-            sq1 = [value][0][0][0]
-            block1 = [value][0][0][1]
-            sq2 = [value][0][1][0]
-            block2 = [value][0][1][1]
+        return None
+
+    def get_random_cardinal(self):
+        return random.choice(self.CARDINALS)
+
+    def is_l_square_danger(self):
+        for key, value in self.L_SQUARE_DICT.items():
+            sq1 = value[0][0]
+            block1 = value[0][1]
+            sq2 = value[1][0]
+            block2 = value[1][1]
 
             if (
                 self.board.squares[key].marker == Square.HUMAN_MARKER
@@ -264,12 +301,13 @@ class TTTGame:
             ):
                 return block2
 
-        # Priority 5: Take corner
-        for square in INTERCARDINALS:
+        return None
+
+    def open_corner(self):
+        for square in self.INTERCARDINALS:
             if self.board.squares[square].is_unused():
                 return square
 
-        # Return None and choose random square (fallback)
         return None
 
     def is_game_over(self):
@@ -298,7 +336,7 @@ class TTTGame:
 
     def play_again(self):
         prompt = input('Play again? (y/n): ').lower()
-        while prompt[0] not in ['y', 'n']:
+        while not prompt or prompt[0] not in 'yn':
             prompt = input('Please enter a valid input (y/n): ').lower()
 
         return prompt[0] == 'y'
